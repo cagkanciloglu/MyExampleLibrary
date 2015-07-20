7a8ed628b9528274031d7d217a29d3569ac4d3f3
Cagkan Ciloglu <cagkan.ciloglu@zomato.com>
2015-07-20 14:12:13 +0300

    pom

--- mylibrary/src/main/java/com/zomato/mylibrary/ExampleUtilliyClass.java
+++ mylibrary/src/main/java/com/zomato/mylibrary/ExampleUtilliyClass.java
@@ -1,5 +1,7 @@
 package com.zomato.mylibrary;
 
+import android.util.Log;
+
 /**
  * Created by Cagkan on 20.7.2015.
  */
@@ -7,6 +9,6 @@ public class ExampleUtilliyClass
 {
     public static void testMethod()
     {
-
+        Log.i("", "");
     }
 }
